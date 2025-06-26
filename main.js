8d13dbbd857daa1a1223c4a2367c99ee13707914
MEANNAJA <atiwittin_i@cmu.ac.th>
2025-06-27 01:14:45 +0700

    5.1-5.5

--- main.js
+++ main.js
@@ -9,7 +9,16 @@ createApp({
         const inStock = ref(true)
         const inventory = ref(100)
         const onSale = ref(true)
-
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
+        const variants = ref([
+            { id: 2234,color: 'green'},
+            { id: 2235,color: 'blue'},
+        ])
+        const sizes = ref(['S','M', 'L'])
         return {
             product,
             description,
@@ -17,7 +26,10 @@ createApp({
             link,
             inStock,
             inventory,
-            onSale
+            onSale,
+            details,
+            variants,
+            sizes
             
         }
     }
