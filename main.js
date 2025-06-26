c5e814eb450c566c0b0140b4a7c456dda9332c6c
MEANNAJA <atiwittin_i@cmu.ac.th>
2025-06-27 02:27:14 +0700

    8.1-8.5

--- main.js
+++ main.js
@@ -1,29 +1,59 @@
-const { createApp, ref } = Vue
+const { createApp, ref , computed} = Vue
 
 createApp({
     setup(){
         const product = ref('Boots')
+        const brand = ref('SE 331')
         const description = ref('Stylish and comfortable boots for handsome')
-        const image = ref('./assets/images/socks_green.jpg')
+        //const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th/')
-        const inStock = ref(false)
+        //const inStock = ref(false)
         const inventory = ref(100)
-        const onSale = ref(true)
+        //const onSale = ref(true)
         const details = ref([
             '50% cotton',
             '30% wool',
             '20% polyester'
         ])
         const variants = ref([
-            { id: 2234,color: 'green',image: './assets/images/socks_green.jpg'},
-            { id: 2235,color: 'blue',image: './assets/images/socks_blue.jpg'},
+            { id: 2234,color: 'green',image: './assets/images/socks_green.jpg', quantity: 50},
+            { id: 2235,color: 'blue',image: './assets/images/socks_blue.jpg', quantity: 0},
         ])
+        const selectedVariant = ref(0)
+
+        function updateVariant(index) {
+            selectedVariant.value = index;
+        }
+
         const sizes = ref(['S','M', 'L'])
         const cart = ref(0)
 
         function addToCart() {
             cart.value += 1
         }
+
+        const title = computed(() => {
+            return brand.value + ' ' + product.value
+        })
+
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image
+        })
+
+        const inStock = computed(() => {
+            return variants.value[selectedVariant.value].quantity
+        })
+        
+        const onSale = computed(() => {
+        return variants.value[selectedVariant.value].quantity > 0
+        })
+
+        const saleMessage = computed(() => {
+        if (onSale.value) {
+            return `${brand.value} ${product.value} is on sale`
+        }
+        return ''
+        })
 
         function removeFromCart() {
             if (cart.value > 0) {
@@ -36,7 +66,7 @@ createApp({
         }
 
         return {
-            product,
+            title,
             description,
             image,
             link,
@@ -49,7 +79,9 @@ createApp({
             cart,
             addToCart,
             removeFromCart,
-            updateImage
+            updateImage,
+            updateVariant,
+            saleMessage
             
         }
     }
