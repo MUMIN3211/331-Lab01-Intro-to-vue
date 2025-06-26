9b1ca775b0ea7f327456779bf5186e9be3d518de
MEANNAJA <atiwittin_i@cmu.ac.th>
2025-06-27 01:27:18 +0700

    6.1-6.7

--- main.js
+++ main.js
@@ -15,10 +15,26 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            { id: 2234,color: 'green'},
-            { id: 2235,color: 'blue'},
+            { id: 2234,color: 'green',image: './assets/images/socks_green.jpg'},
+            { id: 2235,color: 'blue',image: './assets/images/socks_blue.jpg'},
         ])
         const sizes = ref(['S','M', 'L'])
+        const cart = ref(0)
+
+        function addToCart() {
+            cart.value += 1
+        }
+
+        function removeFromCart() {
+            if (cart.value > 0) {
+                cart.value -= 1
+            }
+        }
+
+        function updateImage(variantImage) {
+            image.value = variantImage
+        }
+
         return {
             product,
             description,
@@ -29,7 +45,11 @@ createApp({
             onSale,
             details,
             variants,
-            sizes
+            sizes,
+            cart,
+            addToCart,
+            removeFromCart,
+            updateImage
             
         }
     }
